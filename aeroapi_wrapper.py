f3acc1b60e51074a14c0e5836208fb03d6069d21
Paul Bogard <paulbogard@gmail.com>
2025-02-25 17:04:03 -0800

    Write get_airport_flights

--- aeroapi_wrapper.py
+++ aeroapi_wrapper.py
@@ -1,6 +1,8 @@
 """Class for interacting with AeroAPI."""
-from dotenv import load_dotenv
 import os
+import requests
+import time
+from dotenv import load_dotenv
 
 class AeroAPIWrapper:
     """Class for interacting with AeroAPI version 4."""
@@ -8,8 +10,35 @@ class AeroAPIWrapper:
         load_dotenv()
         self.api_key = os.getenv('API_KEY_AEROAPI_4')
         self.server = "https://aeroapi.flightaware.com/aeroapi"
+        self.isoformat = "%Y-%m-%dT%H:%M:%SZ"
 
         # Set a wait time in seconds to avoid rate limiting on the
         # Personal tier. If your account has a higher rate limit,
         # you can set this to 0.
         self.wait_time = 8
+    
+    def get_airport_flights(
+        self, id, start=None, end=None, cursor=None):
+        """Get flights for a specific airport."""
+        headers = {'x-apikey': self.api_key}
+        params = {
+            'start': self.format_time(start),
+            'end': self.format_time(end),
+            'cursor': cursor,
+        }
+        url = f"{self.server}/airports/{id}/flights"
+        self.wait()
+        response = requests.get(url, headers=headers, params=params)
+        print(f"🌐 Requesting {response.url}")
+        return response.json()
+    
+    def format_time(self, time):
+        """Format time as ISO 8601."""
+        if time is None:
+            return None
+        return time.strftime(self.isoformat)
+    
+    def wait(self):
+        if self.wait_time > 0:
+            print(f"⏳ Waiting {self.wait_time} seconds...")
+            time.sleep(self.wait_time)
