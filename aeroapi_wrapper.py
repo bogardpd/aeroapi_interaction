b07f4f38c00d5d9a2c913d2c96f69f1560e0fd3d
Paul Bogard <paulbogard@gmail.com>
2025-03-19 21:57:50 -0400

    Implement download_fh_recent_flights

--- aeroapi_wrapper.py
+++ aeroapi_wrapper.py
@@ -3,6 +3,7 @@ import os
 import requests
 import time
 from datetime import datetime, timedelta
+from shapely.geometry import LineString, MultiLineString
 from zoneinfo import ZoneInfo
 from dotenv import load_dotenv
 
@@ -18,6 +19,7 @@ class AeroAPIWrapper:
         # Personal tier. If your account has a higher rate limit,
         # you can set this to 0.
         self.wait_time = 8
+        self.wait_until = None
     
     def get_airport_flights(
         self, id, start=None, end=None, cursor=None):
@@ -56,6 +58,15 @@ class AeroAPIWrapper:
         print(f"🌐 Requesting {response.url}")
         return response.json()
     
+    def get_flights(self, ident):
+        """Get flights matching an ident."""
+        headers = {'x-apikey': self.api_key}
+        url = f"{self.server}/flights/{ident}"
+        self.wait()
+        response = requests.get(url, headers=headers)
+        print(f"🌐 Requesting {response.url}")
+        return response.json()
+    
     def format_time(self, time):
         """Format time as ISO 8601."""
         if time is None:
@@ -63,6 +74,75 @@ class AeroAPIWrapper:
         return time.strftime(self.isoformat)
     
     def wait(self):
-        if self.wait_time > 0:
-            print(f"⏳ Waiting {self.wait_time} seconds")
-            time.sleep(self.wait_time)
+        if self.wait_time == 0:
+            return
+        now = datetime.now(tz=ZoneInfo("UTC"))
+        wait_until = now + timedelta(seconds=self.wait_time)
+        if self.wait_until is None or now >= self.wait_until:
+            self.wait_until = wait_until
+            return
+        self.wait_until = wait_until
+        print(f"⏳ Waiting until {self.wait_until}")
+        while datetime.now(tz=ZoneInfo("UTC")) < self.wait_until:
+            time.sleep(1)
+    
+    @staticmethod
+    def split_antimeridian(track_ls: LineString):
+        """Split a LineString at the antimeridian."""
+        crossings = [
+            i + 1 for i, (p1, p2)
+            in enumerate(zip(track_ls.coords[:-1], track_ls.coords[1:]))
+            if (p1[0] < 0 and p2[0] >= 0) or (p1[0] >= 0 and p2[0] < 0)
+        ]
+
+        # Split the track at the indices.
+        if len(crossings) == 0:
+            geom = MultiLineString([track_ls])
+        else:
+            tracks = []
+            starts = [0, *crossings]
+            ends = [*crossings, len(track_ls.coords)-1]
+            tracks = [
+                track_ls.coords[start:end] for start, end in zip(starts, ends)
+            ]
+            for i, track in enumerate(tracks):
+                if i > 0:
+                    p1 = track[0]
+                    p2 = tracks[i-1][-1]
+                    p_cross = AeroAPIWrapper.__crossing_point(p1, p2)
+                    if p_cross is not None:
+                        track.insert(0, p_cross)
+                if i < len(crossings):
+                    p1 = track[-1]
+                    p2 = tracks[i+1][0]
+                    p_cross = AeroAPIWrapper.__crossing_point(p1, p2)
+                    if p_cross is not None:
+                        track.append(p_cross)
+            
+            # Filter out tracks with only one point.
+            tracks = [track for track in tracks if len(track) > 1]
+
+            geom = MultiLineString(tracks)
+        return geom
+    
+    @staticmethod
+    def __crossing_point(p1, p2):
+        """Return the point where a track crosses the antemeridian.
+        Returns None if p1 is already on the antemeridian.
+
+        p1 : tuple(float)
+            The point on the current track
+        p2 : tuple(float)
+            The point on the adjacent track.
+        """
+        p2 = list(p2)
+        if -180 < p1[0] < 0:
+            lon = -180
+            p2[0] = p2[0] - 360
+        elif 0 < p1[0] < 180:
+            lon = 180
+            p2[0] = p2[0] + 360
+        else:
+            return None
+        x_frac = (lon - p1[0]) / (p2[0] - p1[0])
+        return tuple([c1 + (x_frac * (c2 - c1)) for c1, c2 in zip(p1, p2)])
--- download_fh_recent_flights.py
+++ download_fh_recent_flights.py
@@ -3,16 +3,27 @@ Gets recent Flight Historian flights and downloads FlightAware data and
 tracks to a GeoPackage.
 """
 import argparse
+import geopandas as gpd
+import json
 import os
 import requests
-from dotenv import load_dotenv
+from aeroapi_wrapper import AeroAPIWrapper
+from dateutil.parser import isoparse
 from pathlib import Path
+from shapely.geometry import LineString, Point
+from tabulate import tabulate
+
+LAYER = "flight_tracks"
 
 def download_fh_recent_flights(gpkg: Path):
     """Get recent Flight Historian flights and save to GeoPackage."""
 
+    # Get existing IDs.
+    current_gdf = gpd.read_file(gpkg, layer=LAYER)
+    existing_ids = current_gdf['fa_flight_id'].unique().tolist()
+    
     # Get recent flights from Flight Historian.
-    load_dotenv()
+    aw = AeroAPIWrapper()
     api_key_fh = os.getenv("API_KEY_FLIGHT_HISTORIAN")
     headers = {"api-key": api_key_fh}
     url = "https://www.flighthistorian.com/api/recent_flights"
@@ -25,22 +36,98 @@ def download_fh_recent_flights(gpkg: Path):
         cont = input("Would you like to manually enter flight idents? (Y/n) ")
         if cont.lower() != "y":
             return
-        # TODO: Implement manual entry.
-
+        while True:
+            ident = input("Enter a flight ident (e.g., SWA123) or q to quit: ")
+            if ident.lower() == "q":
+                break
+            flight_json = select_flight_from_ident(aw, ident)
+            if flight_json is None:
+                continue
+            fh_id = input("Enter the Flight Historian ID: ")
+            download_flight(aw, gpkg, fh_id, flight_json)
     else:
         print(f"Found {len(json_data)} recent flights.")
         for flight in json_data:
-            print(
-                f"Looking up track for {flight['fa_flight_id']} "
-                f"({flight['departure_utc']})."
-            )
-        download_flight(gpkg, flight['id'], flight['fa_flight_id'])
+            if flight['fa_flight_id'] in existing_ids:
+                print(
+                    f"Skipping {flight['fa_flight_id']} (already downloaded)."
+                )
+                continue
+            flight_json = select_flight_from_ident(aw, flight['fa_flight_id'])
+            if flight_json is None:
+                continue
+            download_flight(aw, gpkg, flight['id'], flight_json)
 
-def download_flight(gpkg: Path, fh_id: int, fa_id: str):
+def download_flight(aw: AeroAPIWrapper, gpkg: Path, fh_id: int, fa_json: str):
     """Download a FlightAware flight and save to GeoPackage."""
-    # TODO: Implement this function.
-    pass
 
+    fa_id = fa_json['fa_flight_id']
+    print(f"Downloading flight {fa_id}.")
+    track_json = aw.get_flight_track(fa_id)
+    if track_json is None:
+        print(f"No track found for {fa_id}.")
+        return
+    
+    track_ls = LineString([Point(
+        p['longitude'],
+        p['latitude'],
+        p['altitude']*30.48 # Convert 100s of feet to meters
+    ) for p in track_json['positions']])
+
+    geom_mls = aw.split_antimeridian(track_ls)
+    dep_utc = aw.format_time(isoparse(fa_json['actual_out']))
+    arr_utc = aw.format_time(isoparse(fa_json['actual_in']))
+    record = {
+        'geom': geom_mls,
+        'departure_utc': dep_utc,
+        'arrival_utc': arr_utc,
+        'fa_flight_id': fa_id,
+        'identifier': fa_json['ident'],
+        'origin_icao': fa_json['origin']['code'],
+        'destination_icao': fa_json['destination']['code'],
+        'fh_id': fh_id,
+        'fa_json': json.dumps(fa_json),
+    }
+    gdf = gpd.GeoDataFrame([record], geometry='geom', crs="EPSG:4326")
+    gdf.to_file(gpkg, layer=LAYER, driver='GPKG', mode='a')
+    print(f"📦 Wrote {fa_id} to {gpkg}")    
+    
+
+def select_flight_from_ident(aw: AeroAPIWrapper, ident: str):
+    """Select a flight from a FlightAware ident and return JSON."""
+    flight_data = aw.get_flights(ident)
+    flights = flight_data.get('flights')
+    if flights is None:
+        print(f"No flights found for {ident}.")
+        return None
+    
+    flight_data = [[
+        str(i + 1),
+        flight['ident'],
+        (
+            f"{flight['origin']['code']}-{flight['destination']['code']}"
+            if (flight['origin'] and flight['destination'])
+            else ""
+        ),
+        flight['actual_out'],
+    ] for i, flight in enumerate(flights)]
+
+    print(f"Found {len(flights)} flights for {ident}.")
+    print(tabulate(
+        flight_data, headers=["#","Ident","Route","Departure"]
+    ))
+
+    flight_row = input("Select row # (or leave blank to skip): ")
+    if flight_row == '' or flight_row.lower() == 'q':
+        return None
+    selected_flight = flights[int(flight_row) - 1]        
+    if selected_flight['actual_out'] is None:
+        print("The selected flight does not have a actual_out time!")
+        return None
+    if selected_flight['actual_in'] is None:
+        print("The selected flight does not have an actual_in time!")
+        return None
+    return selected_flight
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(
